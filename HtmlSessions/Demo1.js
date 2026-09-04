f5431eecf6c85944ff6b2a74205766b574380ab6
Venugopal <mailmevenugopal.a@gmail.com>
2026-09-04 22:13:53 +0530

    4th Sep

---
+++ HtmlSessions/Demo1.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+let x = 100;
+console.log(x);
+let y = 12.45;
+console.log(y);
+let str = "JavaScript Session";
+console.log(str);
+let ch = true;
+console.log(ch);
+
